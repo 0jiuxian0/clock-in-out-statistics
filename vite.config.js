9ec4e3baa10f3be08b56638595031cc68c48f4c5
0jiuxian0 <0jiuxian0@gmail.com>
2025-12-10 09:23:06 +0800

    添加GitHub Actions配置

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/clock-in-out-statistics/' : './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
