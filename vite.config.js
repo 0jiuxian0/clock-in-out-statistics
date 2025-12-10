255ed5e2e112ac59ed530831527ecaaf8cd26d34
0jiuxian0 <0jiuxian0@gmail.com>
2025-12-10 09:30:06 +0800

    修复部署文件

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/clock-in-out-statistics/' : './',
+  base: './', // 默认使用相对路径，GitHub Actions 构建时会通过 --base 参数覆盖
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
